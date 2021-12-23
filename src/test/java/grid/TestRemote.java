e5c6f600e8af54a415dc5e898453493ddb88f1a1
unknown <seleniumcoaching@gmail.com>
2021-12-23 18:38:57 +0530

    first commit

---
+++ src/test/java/grid/TestRemote.java
@@ -0,0 +1,30 @@
+package grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class TestRemote {
+
+	@Test
+	public void googleTest() throws MalformedURLException{
+
+
+		FirefoxOptions opt = new FirefoxOptions();
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://13.233.192.214:4444/"),opt);
+		driver.get("http://google.com");
+		driver.findElement(By.name("q")).sendKeys("Hello Grid !!!");
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+		
+
+	}
+
+}
